ca0836ec4542cc4a45b953fd5b3d325bcb546839
Yuxing Zhang <yzhang@cs.toronto.edu>
2025-07-06 04:22:36 -0400

    reimplemented tuple

--- tuple.cpp
+++ tuple.cpp
@@ -1,38 +1,54 @@
 /*
- * The class template tuple acts as a container for types.
- * For tuple<type_0, type_1, ..., type_{n-1}>,
- * tuple::size = n, the number of types
- * tuple::type<i> = type_i, the i-th type
+ * A basic tuple implementation.
  */
-#include <type_traits>
 
-template<int N, class S, class...T>
-struct get : get<N - 1, T...> {};
-
-template<class S, class...T>
-struct get<0, S, T...> {
-    typedef S type;
-};
-
-template<int N, class S, class... T>
-struct size : size<N + 1, T...> {};
-
-template<int N, class T>
-struct size<N, T> {
-    static const int n = N + 1;
-};
+#include <iostream>
 
 template<class... T>
-struct tuple {
-    static const int size = ::size<0, T...>::n;
-    template<int N>
-    using type = typename get<N, T...>::type;
+struct tuple {};
+
+template<class S, class... T>
+struct tuple<S, T...> : tuple<T...> {
+    S s;
+    tuple(S s, T... t) : s(s), tuple<T...>(t...) {}
 };
 
-typedef tuple<char, int, double> tp;
-static_assert(tp::size == 3, "");
-static_assert(std::is_same_v<tp::type<0>, char>, "");
-static_assert(std::is_same_v<tp::type<1>, int>, "");
-static_assert(std::is_same_v<tp::type<2>, double>, "");
+template<size_t N, class T>
+struct tuple_element;
 
-int main() {}
+template<size_t N, class S, class... T>
+struct tuple_element<N, tuple<S, T...>> : tuple_element<N - 1, tuple<T...>> {};
+
+template<class S, class... T>
+struct tuple_element<0, tuple<S, T...>> { typedef S type; };
+
+template<class T>
+struct tuple_size;
+
+template<class... T>
+struct tuple_size<tuple<T...>> { const static size_t value = sizeof...(T); };
+
+template<class... T>
+struct tuple_size<const tuple<T...>> {
+    const static size_t value = sizeof...(T);
+};
+
+template<size_t N, class S, class... T>
+typename tuple_element<N, tuple<S, T...>>::type& get(tuple<S, T...>& t) {
+    if constexpr (N > 0) { tuple<T...>& t_ = t; return get<N - 1, T...>(t_); }
+    else return t.s;
+}
+
+template<size_t N, class S, class... T>
+const typename tuple_element<N, tuple<S, T...>>::type&
+        get(const tuple<S, T...>& t) {
+    if constexpr (N > 0) {
+        const tuple<T...>& t_ = t; return get<N - 1, T...>(t_);
+    }
+    else return t.s;
+}
+
+int main() {
+    const tuple<int, char, bool> t{0, 'a', true};
+    std::cout << tuple_size<decltype(t)>::value << get<0>(t) << get<1>(t);
+}
